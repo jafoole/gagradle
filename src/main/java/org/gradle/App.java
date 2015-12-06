f602c481c8a1476a550efbb1405e1c292cd3301e
Kevin Galligan <kgalligan@gmail.com>
2015-12-06 17:23:59 -0500

    Where is cool

--- src/main/java/org/gradle/App.java
+++ src/main/java/org/gradle/App.java
@@ -7,16 +7,10 @@ public class App
 {
 	public static void main(String[] args)
 	{
-		concatStrings();
+
 	}
 
-	private static void concatStrings()
-	{
-		String lastBit = "a string";
-		String wholeString = "This is "+ lastBit;
 
-		System.out.println(wholeString);
-	}
 }
 
 
