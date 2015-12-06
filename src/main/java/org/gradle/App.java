d3d2b3de7eb4f037babc052891cd2c05af665f57
Kevin Galligan <kgalligan@gmail.com>
2015-12-06 15:51:11 -0500

    changes

--- src/main/java/org/gradle/App.java
+++ src/main/java/org/gradle/App.java
@@ -1,5 +1,7 @@
 package org.gradle;
 
+import java.util.Scanner;
+
 /**
  * Created by kgalligan on 12/2/15.
  */
@@ -9,7 +11,27 @@ public class App
 
 	public static void main(String[] args) throws InterruptedException
 	{
-		for(int i=-20; i<35; i++)
+
+
+		System.out.println("Ready...");
+		printBlankArrow();
+		Thread.sleep(1200);
+		System.out.println("Aim...");
+		printBlankArrow();
+		Thread.sleep(1200);
+		System.out.println("(and then)");
+		printBlankArrow();
+
+		Scanner inp = new Scanner(System.in);
+		String line;
+
+		while ((line = inp.nextLine()) != null && !line.equalsIgnoreCase("fire"))
+		{
+			System.out.println("Hmm?");
+		}
+		System.out.println("Fire!");
+
+		for(int i=-5; i<30; i++)
 		{
 			StringBuilder stringBuilder = new StringBuilder();
 			if(i<0)
@@ -46,9 +68,21 @@ public class App
 			}
 
 			System.out.println(stringBuilder.toString());
-			Thread.sleep(50);
+			Thread.sleep(70);
 
 		}
+	}
+
+	private static void printBlankArrow()
+	{
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("<}");
+		for (int j = 0; j < 31; j++)
+		{
+			stringBuilder.append(' ');
+		}
+		stringBuilder.append("#");
+		System.out.println(stringBuilder.toString());
 	}
 }
 
