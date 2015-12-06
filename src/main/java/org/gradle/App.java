3258c671a27d09b78c01a6431686e6d49a8af0cc
Kevin Galligan <kgalligan@gmail.com>
2015-12-06 15:44:40 -0500

    Shoot arrow

--- src/main/java/org/gradle/App.java
+++ src/main/java/org/gradle/App.java
@@ -5,10 +5,52 @@ package org.gradle;
  */
 public class App
 {
-	public static void main(String[] args)
+
+
+	public static void main(String[] args) throws InterruptedException
 	{
-		System.out.println("Some java");
+		for(int i=-20; i<35; i++)
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+			if(i<0)
+			{
+				stringBuilder.append("<}");
+				for (int j = 0; j < 31; j++)
+				{
+					stringBuilder.append(' ');
+				}
+				stringBuilder.append("#");
+			}
+			else if(i<29)
+			{
+				stringBuilder.append(" }");
+				for (int j = 0; j < i; j++)
+				{
+					stringBuilder.append(' ');
+				}
+				stringBuilder.append("->");
+				for (int j = i; j < 29; j++)
+				{
+					stringBuilder.append(' ');
+				}
+				stringBuilder.append('#');
+			}
+			else
+			{
+				stringBuilder.append(" }");
+				for (int j = 0; j < 31; j++)
+				{
+					stringBuilder.append(' ');
+				}
+				stringBuilder.append("*");
+			}
+
+			System.out.println(stringBuilder.toString());
+			Thread.sleep(50);
+
+		}
 	}
 }
+
 
 
