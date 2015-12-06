23505691e39887e0606093d475b61a5a22e3e634
Kevin Galligan <kgalligan@gmail.com>
2015-12-06 16:42:23 -0500

    Template for methods example

--- src/main/java/org/gradle/App.java
+++ src/main/java/org/gradle/App.java
@@ -7,16 +7,13 @@ public class App
 {
 	public static void main(String[] args)
 	{
-		concatStrings();
+		//Write a method to get a string input from the user and return it
+
+		//Write a method that takes two numbers as parameters, and prints the product of them
+
+		//Ask the user to enter two numbers, get and convert both, and call your method
 	}
 
-	private static void concatStrings()
-	{
-		String lastBit = "a string";
-		String wholeString = "This is "+ lastBit;
-
-		System.out.println(wholeString);
-	}
 }
 
 
