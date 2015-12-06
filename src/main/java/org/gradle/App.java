f0246b9d49a21bc2f3de2782daba3d93edddfa8f
Kevin Galligan <kgalligan@gmail.com>
2015-12-06 16:48:49 -0500

    Template for methods example

--- src/main/java/org/gradle/App.java
+++ src/main/java/org/gradle/App.java
@@ -7,16 +7,17 @@ public class App
 {
 	public static void main(String[] args)
 	{
-		concatStrings();
+		// write a function that accepts a variable: age
+		// if age is greater than 21, display "Yay, have a beer!"
+		// if age is less than 21, display "Nay, don't have a beer!"
+
+		// ask for the user's age
+
+		// store the user input in a variable: user_age
+
+		// run the function
 	}
 
-	private static void concatStrings()
-	{
-		String lastBit = "a string";
-		String wholeString = "This is "+ lastBit;
-
-		System.out.println(wholeString);
-	}
 }
 
 
